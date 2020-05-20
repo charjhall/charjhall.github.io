b606c4eccaf033839b442891d67402360be49bca
Jackson Hall <jacksonhall@Jacksons-MacBook-Pro-2.local>
2020-05-20 13:13:28 -0400

    Updates

---
+++ precache-manifest.9c783070fb34ff39e64d5a831f1d224f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85027ce48e0867aecc4e7f8753e91d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73666b8c669f4eec505",
+    "url": "/static/css/main.7bfcace7.chunk.css"
+  },
+  {
+    "revision": "0e35567761951b92b9cb",
+    "url": "/static/js/2.2b1a2e34.chunk.js"
+  },
+  {
+    "revision": "d73666b8c669f4eec505",
+    "url": "/static/js/main.40cef2ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f270a660d163ac702367c879cd7ac978",
+    "url": "/static/media/agile.f270a660.png"
+  },
+  {
+    "revision": "db2ebdad62504b81e442b40a677a92b4",
+    "url": "/static/media/familyPicture.db2ebdad.JPG"
+  },
+  {
+    "revision": "9557008f87cc878ab1fdccb679153156",
+    "url": "/static/media/guitar.9557008f.JPG"
+  },
+  {
+    "revision": "12efb1918e4a1f0b8b920e6d32bd74fe",
+    "url": "/static/media/professional_headshot.12efb191.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.2f9b54dddd0c7d318b9ef18ca276e896.js"
+  "/precache-manifest.9c783070fb34ff39e64d5a831f1d224f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
