20a520586b9d725d2300f81e80e1a781cfa722f7
Jackson Hall <jacksonhall@Jacksons-MacBook-Pro-2.local>
2020-05-21 10:35:09 -0400

    Added icons for contact and changed the tab name and favicon

--- src/Contact.js
+++ src/Contact.js
@@ -1,19 +1,39 @@
 import React from "react";
 import "./css/Contact.css";
+import Phone from "./img/phone.png";
+import Email from "./img/email.png";
+import Linkedin from "./img/linkedin.png";
+import Github from "./img/github.png";
 
 export default class Contact extends React.Component {
   render() {
     return (
       <div class="home-content-text">
         <h1>Contact</h1>
-        <h2>Phone: +1 678 588 6214</h2>
-        <h2>Email: jackson.hall@gatech.edu</h2>
-        <a href="https://www.linkedin.com/in/charles-hall-74a73213a/">
-          Linkedin
-        </a>
-        <br />
-        <a href="https://github.com/charjhall">Github</a>
-        <h2>Address: 337347 Georgia Tech Station, Atlanta, Ga, 30332</h2>
+        <div class="row">
+          <div class="column">
+            <a href="tel:678-588-6214">
+              <img src={Phone} alt="Phone" class="center small" />
+            </a>
+          </div>
+          <div class="column">
+            <a href="mailto:jackson.hall@gatech.edu">
+              <img src={Email} alt="Email" class="center small" />
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="column">
+            <a href="https://www.linkedin.com/in/charles-hall-74a73213a/">
+              <img src={Linkedin} alt="Linkedin" class="center small" />{" "}
+            </a>
+          </div>
+          <div class="column">
+            <a href="https://github.com/charjhall">
+              <img src={Github} alt="Github" class="center small" />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
